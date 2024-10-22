1f32127ab1fecad79f5b678523ba54a249475f51
JamesGunsUNI <james.gunner@student.uts.edu.au>
2024-10-23 00:38:01 +1100

    ESTOP GUI
    First attempt to add estop on the gui

--- James/AS2/select_brick_gui.m
+++ James/AS2/select_brick_gui.m
@@ -1,24 +1,41 @@
 function select_brick_gui()
     global selected_brick;
+    global isPaused;
     selected_brick = [];  % Initialize as empty
+    isPaused = false;     % Initialize pause state as false
     
     % Define the brick options
-    brick_names = {'Bread 1', 'Bread 2', 'Bread 3'}; %,'Brick 4', 'Brick 5', 'Brick 6', 'Brick 7', 'Brick 8', 'Brick 9'};
+    brick_names = {'Bread 1', 'Bread 2', 'Bread 3'};
     
     % Create the GUI figure
-    f = figure('Position', [100, 100, 300, 150], 'Name', 'Select Brick to Move');
+    f = figure('Position', [100, 100, 300, 200], 'Name', 'Select Brick to Move');
     
-    % Create a dropdown menu
-    uicontrol('Style', 'text', 'Position', [50, 90, 200, 20], 'String', 'Select a brick to move:');
-    brick_menu = uicontrol('Style', 'popupmenu', 'Position', [50, 70, 200, 20], 'String', brick_names);
+    % Create a dropdown menu for brick selection
+    uicontrol('Style', 'text', 'Position', [50, 140, 200, 20], 'String', 'Select a brick to move:');
+    brick_menu = uicontrol('Style', 'popupmenu', 'Position', [50, 120, 200, 20], 'String', brick_names);
     
-    % Create a button to confirm selection
-    uicontrol('Style', 'pushbutton', 'Position', [100, 30, 100, 30], 'String', 'Move Brick', 'Callback', @move_selected_brick);
+    % Create a button to confirm brick selection
+    uicontrol('Style', 'pushbutton', 'Position', [100, 80, 100, 30], 'String', 'Move Brick', 'Callback', @move_selected_brick);
+    
+    % Create a button to pause/resume the simulation
+    pause_button = uicontrol('Style', 'pushbutton', 'Position', [100, 40, 100, 30], 'String', 'Pause Simulation', 'Callback', @pauseSimulation);
     
     % Callback function to set the selected brick and close the GUI
     function move_selected_brick(~, ~)
         selected_brick = brick_menu.Value;  % Store the selected brick index in the global variable
         disp(['You selected ', brick_names{selected_brick}]);
-        close(f);  % Close the GUI
+     end
+
+    % Callback function for pausing and resuming the simulation
+    function pauseSimulation(~, ~)
+        if isPaused
+            isPaused = false;  % Resume the simulation
+            set(pause_button, 'String', 'Pause Simulation');  % Update button text
+            disp('Simulation Resumed');
+        else
+            isPaused = true;   % Pause the simulation
+            set(pause_button, 'String', 'Resume Simulation');  % Update button text
+            disp('Simulation Paused');
+        end
     end
 end
