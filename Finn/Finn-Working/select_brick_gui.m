868f661aa04ba17a151bcb5016196e64226645b1
Finn Witney <finn.a.witney@student.uts.edu.au>
2024-10-26 22:34:16 +1100

    improved gui

--- Finn/Finn-Working/select_brick_gui.m
+++ Finn/Finn-Working/select_brick_gui.m
@@ -1,53 +1,64 @@
 function select_brick_gui()
-    global selected_brick;
-    global iiwa_movement_requested;
-    global isPaused;
-    selected_brick = [];       % Initialize as empty
-    iiwa_movement_requested = false; % Initialize IIWA move request
-    isPaused = false;          % Initialize pause state as false
+    global selected_brick iiwa_movement_requested isPaused movement_requested;
+    selected_brick = [];
+    iiwa_movement_requested = false;
+    isPaused = false;
+    movement_requested = false;
 
     % Define the brick options
     brick_names = {'Bread 1', 'Bread 2', 'Bread 3'};
 
     % Create the GUI figure
-    f = figure('Position', [100, 100, 300, 250], 'Name', 'Select Brick to Move');
+    f = figure('Position', [100, 100, 300, 300], 'Name', 'Select Brick and Move');
 
-    % Create a dropdown menu for brick selection
-    uicontrol('Style', 'text', 'Position', [50, 180, 200, 20], 'String', 'Select a brick to move:');
-    brick_menu = uicontrol('Style', 'popupmenu', 'Position', [50, 160, 200, 20], 'String', brick_names);
+    % Dropdown menu for brick selection
+    uicontrol('Style', 'text', 'Position', [50, 220, 200, 20], 'String', 'Select a brick to move:');
+    brick_menu = uicontrol('Style', 'popupmenu', 'Position', [50, 200, 200, 20], 'String', brick_names);
 
-    % Create a button to confirm brick selection
-    uicontrol('Style', 'pushbutton', 'Position', [50, 120, 200, 30], 'String', 'Move Brick', 'Callback', @move_selected_brick);
+    % Button to confirm brick selection
+    uicontrol('Style', 'pushbutton', 'Position', [50, 160, 200, 30], 'String', 'Select Brick', 'Callback', @select_brick);
 
-    % Create a button to move the IIWA robot
-    uicontrol('Style', 'pushbutton', 'Position', [50, 80, 200, 30], 'String', 'Move IIWA', 'Callback', @move_iiwa);
+    % Button to move IIWA
+    uicontrol('Style', 'pushbutton', 'Position', [50, 120, 200, 30], 'String', 'Move IIWA', 'Callback', @request_iiwa_move);
 
-    % Create a button to pause/resume the simulation
+    % "Go" button to start movement sequence
+    go_button = uicontrol('Style', 'pushbutton', 'Position', [50, 80, 200, 30], 'String', 'Go', 'Enable', 'off', 'Callback', @start_movement);
+
+    % Pause/resume button
     pause_button = uicontrol('Style', 'pushbutton', 'Position', [50, 40, 200, 30], 'String', 'Pause Simulation', 'Callback', @pauseSimulation);
 
-    % Callback function to set the selected brick
-    function move_selected_brick(~, ~)
-        selected_brick = brick_menu.Value;  % Store selected brick index
+    function select_brick(~, ~)
+        selected_brick = brick_menu.Value;
         disp(['You selected ', brick_names{selected_brick}]);
-        close(f); % Close the GUI after selection
+        enableGoButton();
     end
 
-    % Callback function to set the IIWA move request flag
-    function move_iiwa(~, ~)
-        iiwa_movement_requested = true;  % Set the global IIWA movement flag
+    function request_iiwa_move(~, ~)
+        iiwa_movement_requested = true;
         disp('IIWA movement requested');
-        close(f); % Close the GUI after IIWA request
+        enableGoButton();
     end
 
-    % Callback function for pausing and resuming the simulation
+    function enableGoButton()
+        if ~isempty(selected_brick) && iiwa_movement_requested
+            set(go_button, 'Enable', 'on');
+        end
+    end
+
+    function start_movement(~, ~)
+        movement_requested = true;
+        disp('Movement sequence initiated.');
+        close(f);
+    end
+
     function pauseSimulation(~, ~)
         if isPaused
-            isPaused = false;  % Resume the simulation
-            set(pause_button, 'String', 'Pause Simulation');  % Update button text
+            isPaused = false;
+            set(pause_button, 'String', 'Pause Simulation');
             disp('Simulation Resumed');
         else
-            isPaused = true;   % Pause the simulation
-            set(pause_button, 'String', 'Resume Simulation');  % Update button text
+            isPaused = true;
+            set(pause_button, 'String', 'Resume Simulation');
             disp('Simulation Paused');
         end
     end
